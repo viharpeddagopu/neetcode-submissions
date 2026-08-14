5d420e8ccfff8c397ea4eb8fdb9b95f98311984e
Peddagopu Keerthi Vihar <viharpeddagopu@gmail.com>
2026-08-14 11:44:36 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while(l <= r){
+            int mid = (r - l)/2 + l;
+
+            if(target == nums[mid])
+                return mid;
+            else if(target > nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+}
